46f0838b0206d227485deee9311d07cd2e418e3f
kobayang <naokiod@gmail.com>
2017-10-20 20:04:53 +0900

    Show image ids

---
+++ private_isu/webapp/ruby/tasks/retrive_images.rb
@@ -0,0 +1,35 @@
+require 'mysql2'
+
+def config
+  @config ||= {
+    db: {
+      host: ENV['ISUCONP_DB_HOST'] || 'localhost',
+      port: ENV['ISUCONP_DB_PORT'] && ENV['ISUCONP_DB_PORT'].to_i,
+      username: ENV['ISUCONP_DB_USER'] || 'root',
+      password: ENV['ISUCONP_DB_PASSWORD'],
+      database: ENV['ISUCONP_DB_NAME'] || 'isuconp',
+    },
+  }
+end
+
+def db
+  return Thread.current[:isuconp_db] if Thread.current[:isuconp_db]
+  client = Mysql2::Client.new(
+    host: config[:db][:host],
+    port: config[:db][:port],
+    username: config[:db][:username],
+    password: config[:db][:password],
+    database: config[:db][:database],
+    encoding: 'utf8mb4',
+    reconnect: true,
+  )
+  client.query_options.merge!(symbolize_keys: true, database_timezone: :local, application_timezone: :local)
+  Thread.current[:isuconp_db] = client
+  client
+end
+
+image_ids = db.query('SELECT `id` FROM posts LIMIT 10')
+
+image_ids.to_a.each do |image_id|
+  p image_id
+end
